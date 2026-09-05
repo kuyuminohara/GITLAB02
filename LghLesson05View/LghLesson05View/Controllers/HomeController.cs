7f96ae9d64a633c17ad4b0ab1edf06be318a1f91
Le Gia Hung <thanhck123z@gmail.com>
2026-09-05 08:36:03 +0700

    Lgh-Lesson05View

---
+++ LghLesson05View/LghLesson05View/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using LghLesson05View.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LghLesson05View.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult RazorCode()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LghLesson05View/LghLesson05View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LghLesson05View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
